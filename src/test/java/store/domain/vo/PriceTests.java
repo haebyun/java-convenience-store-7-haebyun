3c8666a1d9dbe7b2456b38aeedd77e04576e60d4
haebyun <bhk4392@gmail.com>
2024-11-08 14:32:26 +0900

    test(Price): 상품의 가격은 0보다 커야 한다는 것에 대한 예외 테스트

--- src/test/java/store/domain/vo/PriceTests.java
+++ src/test/java/store/domain/vo/PriceTests.java
@@ -3,6 +3,8 @@ package store.domain.vo;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import store.domain.product.vo.Price;
 
 public class PriceTests {
@@ -15,5 +17,13 @@ public class PriceTests {
         Price price = Price.of(value);
 
         Assertions.assertThat(price.getValue()).isEqualTo(value);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1000, -100, 0})
+    @DisplayName("상품의 가격은 0보다 커야 한다.")
+    void testsInvalidPriceValue(int value) {
+        Assertions.assertThatThrownBy(() -> Price.of(value))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
