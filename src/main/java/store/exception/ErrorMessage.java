001e439b6f15358319ebc5655565bb6a1652bb9d
haebyun <bhk4392@gmail.com>
2024-11-08 15:53:14 +0900

    feat(ErrorMessage): 재고 수량과 관련한 예외 메시지 추가

--- src/main/java/store/exception/ErrorMessage.java
+++ src/main/java/store/exception/ErrorMessage.java
@@ -1,7 +1,8 @@
 package store.exception;
 
 public enum ErrorMessage {
-    INVALID_PRICE("상품의 가격은 양수만 허용됩니다.");
+    INVALID_PRICE("상품의 가격은 양수만 허용됩니다."),
+    INVALID_STOCK_QUANTITY("재고 수량은 0 이상만 허용됩니다.");
 
     private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
 
