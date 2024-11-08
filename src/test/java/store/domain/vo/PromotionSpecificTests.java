62baae6166477960911e8d5ad5c9f26438b2cca9
haebyun <bhk4392@gmail.com>
2024-11-09 01:48:52 +0900

    test(PromotionSpecific): 구매 수량에 따른 무료 제공 횟수에 대한 정상 작동 테스트와 예외 상황 테스트

--- src/test/java/store/domain/vo/PromotionSpecificTests.java
+++ src/test/java/store/domain/vo/PromotionSpecificTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PromotionSpecificTests {
 
@@ -23,8 +24,37 @@ public class PromotionSpecificTests {
             "2, -1"
     })
     @DisplayName("buy가 1 이상이 아니거나 get이 0 미만이면 예외가 발생한다.")
-    void testInvalidPromotionSpecificCreation(int buy, int get) {
+    void testsInvalidPromotionSpecificCreation(int buy, int get) {
         assertThatThrownBy(() -> PromotionSpecific.of(buy, get))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, 1, 2, 1",    // 2+1 프로모션 조건 충족
+            "2, 1, 4, 2",    // 조건을 두 번 충족하는 경우
+            "2, 1, 5, 2",    // 조건 충족 이후 나머지가 있는 경우
+            "1, 1, 6, 6",    // 1+1 프로모션
+            "3, 2, 7, 4",    // 조건 충족 이후 나머지가 있는 경우
+            "3, 2, 6, 4"     // 3+2 프로모션
+    })
+    @DisplayName("구매 수량에 따라 무료 제공 수량을 계산한다.")
+    void testsCalculateFreeCount(int buy, int get, int purchasedQuantity, int expectedBonus) {
+        PromotionSpecific promotionSpecific = PromotionSpecific.of(buy, get);
+
+        int bonus = promotionSpecific.calculateFreeCount(purchasedQuantity);
+
+        assertThat(bonus).isEqualTo(expectedBonus);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -5})
+    @DisplayName("구매 수량이 buy 조건에 미달하면 무료 제공 수량은 0이다.")
+    void testsCalculateBonusWhenPurchasedQuantityIsSmall(int purchasedQuantity) {
+        PromotionSpecific promotionSpecific = PromotionSpecific.of(3, 1);
+
+        int bonus = promotionSpecific.calculateFreeCount(purchasedQuantity);
+
+        assertThat(bonus).isEqualTo(0);
     }
 }
