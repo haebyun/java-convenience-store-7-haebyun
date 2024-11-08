da2eee5ec8d88ffcdbcfff790d6f49623dc28e0e
haebyun <bhk4392@gmail.com>
2024-11-08 16:14:13 +0900

    test(Stock): 재고 감소 로직에서 예외가 발생하는 부분 테스트
    - 재고 감소 수량이 0 이하인 경우
    - 재고 감소 수량이 현재 재고 수량보다 많은 경우

--- src/test/java/store/domain/vo/StockTests.java
+++ src/test/java/store/domain/vo/StockTests.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class StockTests {
-    private final Integer initialQuantity = 100;
+    private static final Integer initialQuantity = 100;
 
     @Test
     @DisplayName("재고 수량은 재고의 갯수를 나타내는 필드를 갖는다.")
@@ -35,5 +35,27 @@ public class StockTests {
         stock.decrease(amount);
 
         assertThat(stock.getQuantity()).isEqualTo(initialQuantity - amount);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -4, -10})
+    @DisplayName("재고 감소 수량이 0 이하인 경우 예외가 발생한다.")
+    void testsInvalidDecreaseStockQuantityThenThrowsException(int amount) {
+        Stock stock = Stock.of(initialQuantity);
+
+        assertThatThrownBy(() -> stock.decrease(amount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    @DisplayName("재고 감소 수량이 현재 재고 수량보다 많은 경우 예외가 발생한다.")
+    void testsInvalidToLargeDecreaseStockQuantityThenThrowsException(int amount) {
+        Stock stock = Stock.of(initialQuantity);
+
+        int toLargeDecreaseAmount = amount + initialQuantity;
+
+        assertThatThrownBy(() -> stock.decrease(toLargeDecreaseAmount))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
