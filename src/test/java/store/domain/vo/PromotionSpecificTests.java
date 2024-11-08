cd08e538c853cce203e3ff43b4c666b862b0a476
haebyun <bhk4392@gmail.com>
2024-11-09 01:30:25 +0900

    test(PromotionSpecific): buy가 1 이상이 아니거나 get이 0 미만인 경우 예외 발생 테스트

--- src/test/java/store/domain/vo/PromotionSpecificTests.java
+++ src/test/java/store/domain/vo/PromotionSpecificTests.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class PromotionSpecificTests {
 
@@ -12,5 +14,17 @@ public class PromotionSpecificTests {
     void testCreatePromotionSpecific() {
         PromotionSpecific promotionSpecific = PromotionSpecific.of(2, 1);
         assertThat(promotionSpecific).isNotNull();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1",
+            "-1, 1",
+            "2, -1"
+    })
+    @DisplayName("buy가 1 이상이 아니거나 get이 0 미만이면 예외가 발생한다.")
+    void testInvalidPromotionSpecificCreation(int buy, int get) {
+        assertThatThrownBy(() -> PromotionSpecific.of(buy, get))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
