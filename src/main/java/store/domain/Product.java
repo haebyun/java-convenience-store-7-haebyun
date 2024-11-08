67cc183d88d619842d2dbb0941c1fed1dca09f7c
haebyun <bhk4392@gmail.com>
2024-11-08 17:15:31 +0900

    refactor(Product): Stock을 Product가 관리하도록 수정 및 패키지 위치 변경

---
+++ src/main/java/store/domain/Product.java
@@ -0,0 +1,38 @@
+package store.domain;
+
+import store.domain.vo.Price;
+import store.domain.vo.Stock;
+
+public class Product {
+    private final String name;
+    private final Price price;
+    private final Stock stock;
+    private final String promotionName;
+
+    private Product(String name, Price price, Stock stock, String promotionName) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.promotionName = promotionName;
+    }
+
+    public static Product of(String name, Integer price, Integer stockQuantity, String promotionName) {
+        return new Product(name, Price.of(price), Stock.of(stockQuantity), promotionName);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPriceValue() {
+        return price.getValue();
+    }
+
+    public Integer getStockValue() {
+        return stock.getQuantity();
+    }
+
+    public String getPromotionName() {
+        return promotionName;
+    }
+}
--- src/test/java/store/domain/ProductTests.java
+++ src/test/java/store/domain/ProductTests.java
@@ -1,4 +1,4 @@
-package store.domain.vo;
+package store.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -8,16 +8,18 @@ import org.junit.jupiter.api.Test;
 public class ProductTests {
 
     @Test
-    @DisplayName("상품은 이름, 가격, 적용 프로모션 이름을 갖는다.")
+    @DisplayName("상품은 이름, 가격, 재고 수량, 적용 프로모션 이름을 갖는다.")
     void testCreateProduct() {
         String productName = "요아정";
         Integer productPrice = 2500;
+        Integer stockQuantity = 100;
         String promotionName = "2+1";
 
-        Product product = Product.of(productName, productPrice, promotionName);
+        Product product = Product.of(productName, productPrice, stockQuantity, promotionName);
 
         assertThat(product.getName()).isEqualTo(productName);
         assertThat(product.getPriceValue()).isEqualTo(productPrice);
+        assertThat(product.getStockValue()).isEqualTo(stockQuantity);
         assertThat(product.getPromotionName()).isEqualTo(promotionName);
     }
 }
