9ecf181010b572976f9e7ac81ae7cf8d10f82640
haebyun <bhk4392@gmail.com>
2024-11-11 14:18:34 +0900

    feat(Promotions): 상품 이름, 가격, 프로모션 적용된 재고, 무료 증정 갯수를 필드로 가지는 PromotionResult를 반환하는 로직 구현

--- src/main/java/store/domain/Promotions.java
+++ src/main/java/store/domain/Promotions.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import store.domain.vo.PromotionData;
 import store.domain.vo.PromotionProductInfo;
+import store.domain.vo.PromotionResult;
 
 public class Promotions {
     private final List<Promotion> promotions;
@@ -34,5 +35,18 @@ public class Promotions {
         int extraStock = promotion.getAdditionalFreeItemCount(info.stock(), orderQuantity);
         int nonPromotionQuantity = promotion.calculateNonPromotionQuantity(info.stock(), orderQuantity);
         return new PromotionData(extraStock, nonPromotionQuantity);
+    }
+
+    public PromotionResult createPromotionResult(PromotionProductInfo info, Integer orderQuantity, LocalDate orderDate) {
+        return getByName(info.promotionName())
+                .filter(promo -> promo.isActive(orderDate))
+                .map(promo -> buildResult(promo, info, orderQuantity))
+                .orElse(new PromotionResult(info.name(), info.price(), 0, 0));
+    }
+
+    private PromotionResult buildResult(Promotion promotion, PromotionProductInfo info, Integer orderQuantity) {
+        int appliedQuantity = promotion.calculateAppliedPromotionQuantity(info.stock(), orderQuantity);
+        int freeQuantity = promotion.calculateFreeProductCount(appliedQuantity);
+        return new PromotionResult(info.name(), info.price(), appliedQuantity, freeQuantity);
     }
 }
