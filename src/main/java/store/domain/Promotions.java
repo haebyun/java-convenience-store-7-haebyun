e6d8326ee17ca561a36ec48ccacaa43a2bdbc5ed
haebyun <bhk4392@gmail.com>
2024-11-11 13:50:04 +0900

    feat(Promotions): PromotionData라는 객체를 만들어서 반환 하는 기능 구현
    - PromotionData는 무료 증정을 위한 추가적으로 필요한 갯수, 모든 금액을 내야하는 갯수를 필드로 가진다.

--- src/main/java/store/domain/Promotions.java
+++ src/main/java/store/domain/Promotions.java
@@ -1,6 +1,10 @@
 package store.domain;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import store.domain.vo.PromotionData;
+import store.domain.vo.PromotionProductInfo;
 
 public class Promotions {
     private final List<Promotion> promotions;
@@ -11,5 +15,24 @@ public class Promotions {
 
     public static Promotions of(List<Promotion> promotions) {
         return new Promotions(promotions);
+    }
+
+    private Optional<Promotion> getByName(String name) {
+        return promotions.stream()
+                .filter(promo -> name.equals(promo.getName()))
+                .findAny();
+    }
+
+    public PromotionData createPromotionData(PromotionProductInfo info, Integer orderQuantity, LocalDate orderDate) {
+        return getByName(info.promotionName())
+                .filter(promotion -> promotion.isActive(orderDate))
+                .map(promotion -> buildData(promotion, info, orderQuantity))
+                .orElse(new PromotionData(0, 0));
+    }
+
+    private PromotionData buildData(Promotion promotion, PromotionProductInfo info, Integer orderQuantity) {
+        int extraStock = promotion.getAdditionalFreeItemCount(info.stock(), orderQuantity);
+        int nonPromotionQuantity = promotion.calculateNonPromotionQuantity(info.stock(), orderQuantity);
+        return new PromotionData(extraStock, nonPromotionQuantity);
     }
 }
