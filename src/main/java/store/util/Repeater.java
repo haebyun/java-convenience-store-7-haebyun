ccbdf2c18cbf838299611095d24a15a70509b2b7
haebyun <bhk4392@gmail.com>
2024-11-11 17:46:44 +0900

    feat(Repeater): 조건에 맞을 때 까지 실행을 반복하는 기능을 가진 클래스 구현

---
+++ src/main/java/store/util/Repeater.java
@@ -0,0 +1,22 @@
+package store.util;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public final class Repeater {
+    public static <T> T executeWithRetry(Supplier<T> supplier, Consumer<String> errorHandler) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                errorHandler.accept(e.getMessage());
+            }
+        }
+    }
+
+    public static void executeWhileTrue(Runnable action, Supplier<Boolean> orderStop) {
+        do {
+            action.run();
+        } while (!orderStop.get());
+    }
+}
