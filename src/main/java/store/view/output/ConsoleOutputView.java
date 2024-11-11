49950326685658b23de5ed568e4b7640dd5b8c00
haebyun <bhk4392@gmail.com>
2024-11-11 18:57:11 +0900

    feat(ConsoleOutputView): 환영 인사와 함께 상품명, 가격, 프로모션 이름, 재고를 안내한다. 만약 재고가 0개라면 재고 없음을 출력하는 기능 구현

--- src/main/java/store/view/output/ConsoleOutputView.java
+++ src/main/java/store/view/output/ConsoleOutputView.java
@@ -1,8 +1,17 @@
 package store.view.output;
 
+import store.domain.product.Products;
+import store.view.output.formatter.OutputFormatter;
+
 public class ConsoleOutputView implements OutputView{
     @Override
     public void printError(String errorMessage) {
         System.out.println(errorMessage + "\n");
+    }
+
+    @Override
+    public void printProductsStocks(Products products) {
+        String formattedStocks = OutputFormatter.formatProductsStocks(products);
+        System.out.println(formattedStocks);
     }
 }
