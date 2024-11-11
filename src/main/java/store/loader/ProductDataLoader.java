5c896870b7e8bb6bf4a9d31bc0d8a8237390ed53
haebyun <bhk4392@gmail.com>
2024-11-11 15:27:13 +0900

    feat(ProductDataLoader): src/main/resources/products.md 파일을 이용하여 상품 정보를 받도록 클래스 구현
    - List<ProductDTO> 형태로 반환한다.
    - 만약 PromotionName이 'null'인 경우 "No Promotion"을 저장한다.

---
+++ src/main/java/store/loader/ProductDataLoader.java
@@ -0,0 +1,56 @@
+package store.loader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import store.dto.ProductDTO;
+import store.exception.ErrorMessage;
+
+public class ProductDataLoader {
+    public List<ProductDTO> loadProducts(String fileName) {
+        try (BufferedReader reader = createBufferedReader(fileName)) {
+            return readProductData(reader);
+        } catch (IOException | NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.OTHER_INVALID_INPUT.getMessage());
+        }
+    }
+
+    private BufferedReader createBufferedReader(String fileName) throws IOException {
+        InputStream is = getFileInputStream(fileName);
+        return new BufferedReader(new InputStreamReader(is));
+    }
+
+    private InputStream getFileInputStream(String fileName) {
+        return getClass().getClassLoader().getResourceAsStream(fileName);
+    }
+
+    private List<ProductDTO> readProductData(BufferedReader reader) throws IOException {
+        List<ProductDTO> productDTOs = new ArrayList<>();
+        String line;
+        boolean isFirstLine = true;
+        while ((line = reader.readLine()) != null) {
+            if (isFirstLine) {
+                isFirstLine = false;
+                continue;
+            }
+            productDTOs.add(parseProductInfo(line));
+        }
+        return productDTOs;
+    }
+
+    private ProductDTO parseProductInfo(String line) {
+        String[] parts = line.split(",", -1);
+        String name = parts[0].trim();
+        int price = Integer.parseInt(parts[1].trim());
+        int quantity = Integer.parseInt(parts[2].trim());
+        String promotion = parts[3].trim();
+
+        if (promotion.equalsIgnoreCase("null") || promotion.isEmpty()) {
+            promotion = "No Promotion";
+        }
+        return new ProductDTO(name, price, quantity, promotion);
+    }
+}
