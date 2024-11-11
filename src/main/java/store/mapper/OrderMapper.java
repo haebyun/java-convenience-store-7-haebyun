a4360dbf89f64ee7078c06d4d3614313ccd95b9f
haebyun <bhk4392@gmail.com>
2024-11-11 15:42:03 +0900

    feat(OrderMapper): List<PromotionDTO> 를 Promotions로 매핑하는 기능 구현

--- src/main/java/store/mapper/OrderMapper.java
+++ src/main/java/store/mapper/OrderMapper.java
@@ -3,7 +3,10 @@ package store.mapper;
 import java.util.List;
 import store.domain.product.Product;
 import store.domain.product.Products;
+import store.domain.promotion.Promotion;
+import store.domain.promotion.Promotions;
 import store.dto.ProductDTO;
+import store.dto.PromotionDTO;
 
 public final class OrderMapper {
     public static Products toProductsDomain(List<ProductDTO> productDTOs) {
@@ -16,5 +19,18 @@ public final class OrderMapper {
                 )
                 .toList();
         return Products.of(products);
+    }
+
+    public static Promotions toPromotionsDomain(List<PromotionDTO> promotionDTOs) {
+        List<Promotion> promotions = promotionDTOs.stream()
+                .map(promotionDTO -> Promotion.from(
+                        promotionDTO.name(),
+                        promotionDTO.buy(),
+                        promotionDTO.get(),
+                        promotionDTO.startDate(),
+                        promotionDTO.endDate()
+                ))
+                .toList();
+        return Promotions.of(promotions);
     }
 }
