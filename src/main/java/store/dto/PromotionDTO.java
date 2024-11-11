2d2ca1ab0cb33f4414af58ccd73ffb9863db4e65
haebyun <bhk4392@gmail.com>
2024-11-11 15:32:45 +0900

    feat(PromotionDTO): 프로모션 이름, 프로모션 조건 및 혜택 정보, 시작 날짜와 종료 날짜를 필드로 가지는 DTO 레코드 구현

---
+++ src/main/java/store/dto/PromotionDTO.java
@@ -0,0 +1,21 @@
+package store.dto;
+
+import java.time.LocalDate;
+
+public record PromotionDTO(
+        String name,
+        Integer buy,
+        Integer get,
+        LocalDate startDate,
+        LocalDate endDate
+) {
+    public static PromotionDTO of(
+            String name,
+            Integer buy,
+            Integer get,
+            LocalDate startDate,
+            LocalDate endDate
+    ) {
+        return new PromotionDTO(name, buy, get, startDate, endDate);
+    }
+}
