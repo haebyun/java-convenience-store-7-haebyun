f78f0e8d32300ef7ddbaaafe02aec33c45ae0e82
haebyun <bhk4392@gmail.com>
2024-11-11 22:57:10 +0900

    refactor(MembershipCalculator): Order를 파라미터로 받는 것이 아닌 totalAmountOfMembershipTarget을 파라미터로 받도록 수정

--- src/main/java/store/domain/membership/MembershipCalculator.java
+++ src/main/java/store/domain/membership/MembershipCalculator.java
@@ -1,14 +1,10 @@
 package store.domain.membership;
 
-import store.domain.order.Order;
-
 public class MembershipCalculator {
     private static final double DISCOUNT_RATE = 0.3; // 30% 할인율
     private static final int MAX_DISCOUNT = 8000; // 최대 할인 금액
 
-    public Integer calculateMembershipDiscount(Order order) {
-        Integer totalAmountOfMembershipTarget = order.calculateMembershipTargetAmount();
-
+    public Integer calculateMembershipDiscount(Integer totalAmountOfMembershipTarget) {
         int discount = (int) (totalAmountOfMembershipTarget * DISCOUNT_RATE);
         return Math.min(discount, MAX_DISCOUNT);
     }
